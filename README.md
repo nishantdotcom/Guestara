89c77699a68a9483594a610887bfff4a6ffe507e
nishantdotcom <nishantsingh1308@gmail.com>
2024-05-16 12:56:40 +0530

    update subcategory and item api

--- README.md
+++ README.md
@@ -11,4 +11,18 @@
 3. Get category with id or name
    GET => /v1/category/search?id=1&name=nishant
 
-4.
+4. Patch (update) data with specific category id
+
+   PATCH => v1/category/:categoryId
+
+5. POST (create ) subcategory under a category
+
+POST =>v1/category/:categoryId/subcategory
+
+6. GET all sub categories
+
+GET => v1/category/subcategory
+
+7. GET all the subcategory from id or name
+
+GET =>/v1/subcategory/search?id=1&name=Test123
--- src/router/v1.ts
+++ src/router/v1.ts
@@ -109,7 +109,7 @@ v1.get("/category/search", async (req, res) => {
   }
 });
 //UPDATE CATEGORY with category id
-v1.patch("category/:categoryId", async (req, res) => {
+v1.patch("/category/:categoryId", async (req, res) => {
   const id = parseInt(req.params.categoryId);
   const { name, imageurl, description, taxApplicablity, taxNumber, taxType } =
     req.body;
@@ -135,6 +135,176 @@ v1.patch("category/:categoryId", async (req, res) => {
       msg: "Getting category",
       status: 1,
       data: updateProfile,
+    });
+  } catch (err) {
+    res.status(403).send({
+      msg: "error in getting category !!",
+      status: 0,
+      err: err,
+    });
+  }
+});
+// POST Sub category of a  given category id
+v1.post("/category/:categoryId/subcategory", async (req, res) => {
+  const id = parseInt(req.params.categoryId);
+  const { name, image, description, taxApplicability, taxNumber } = req.body;
+  // console.log(req.body);
+  let valid: boolean = true;
+  if (taxApplicability == 0) {
+    valid = false;
+  }
+  try {
+    const createSubcategory = await prisma.subCategory.create({
+      data: {
+        name: name,
+        image: image,
+        description: description,
+        taxApplicability: valid,
+        taxNumber: taxNumber,
+        categoryId: id,
+      },
+    });
+
+    res.status(200).send({
+      msg: "sucess create subcategory !!",
+      data: createSubcategory,
+      status: 1,
+    });
+  } catch (err) {
+    res.status(403).send({
+      msg: "error in saving subcategory !!",
+      status: 0,
+      err: err,
+    });
+  }
+});
+
+// GET ALL SUBCATEGORIES
+
+v1.get("/category/subcategory", async (req, res) => {
+  try {
+    const getAllsubcat = await prisma.subCategory.findMany({});
+    res.status(200).send({
+      msg: "All subcategories fetched !!!",
+      data: getAllsubcat,
+      status: 1,
+    });
+  } catch (err) {
+    res.status(403).send({
+      msg: "error in getting all subcategory !!",
+      status: 0,
+      err: err,
+    });
+  }
+});
+// GET ALL SUBCATEGORY FO SPECIFIC CATEGORY
+
+v1.get("/category/subcategory/:catgoryId", async (req, res) => {
+  const id = parseInt(req.params.catgoryId);
+  try {
+    const getAllsubCategoryofCategory = await prisma.subCategory.findMany({
+      where: {
+        categoryId: id,
+      },
+    });
+    res.status(200).send({
+      data: getAllsubCategoryofCategory,
+      status: 1,
+      msg: "Getting all sub cats",
+    });
+  } catch (err) {
+    res.status(403).send({
+      msg: "error in getting subcategory !!",
+      status: 0,
+      err: err,
+    });
+  }
+});
+//GET SUBCATEGORY SEARCH BY NAME OR ID
+v1.get("/subcategory/search", async (req, res) => {
+  const id: any = req.query.id;
+  const name: any = req.query.name;
+
+  if (id) {
+    const finalId: number = parseInt(id);
+    try {
+      const categoryProfile = await prisma.subCategory.findUnique({
+        where: {
+          id: finalId,
+        },
+      });
+
+      res.status(200).send({
+        msg: "Getting category",
+        status: 1,
+        data: categoryProfile,
+      });
+    } catch (err) {
+      res.status(403).send({
+        msg: "error in getting subcategory !!",
+        status: 0,
+        err: err,
+      });
+    }
+  } else if (name) {
+    try {
+      const categoryProfile = await prisma.subCategory.findMany({
+        where: {
+          name: name,
+        },
+      });
+
+      res.status(200).send({
+        msg: "Getting category",
+        status: 1,
+        data: categoryProfile,
+      });
+    } catch (err) {
+      res.status(403).send({
+        msg: "error in getting subcategory !!",
+        status: 0,
+        err: err,
+      });
+    }
+  }
+});
+//CREATE ITEM FOR AN SUBCATEGORY
+
+v1.post("/subcategory/:subcategoryId/item", async (req, res) => {
+  const id = parseInt(req.params.subcategoryId);
+  const {
+    name,
+    image,
+    description,
+    taxNumber,
+    baseAmount,
+    discount,
+    totalAmount,
+    taxApplicablity,
+  } = req.body;
+
+  let valid: boolean = true;
+  if (taxApplicablity == 0) {
+    valid = false;
+  }
+  try {
+    const itemdata = await prisma.items.create({
+      data: {
+        name: name,
+        image: image,
+        description: description,
+        taxNumber: taxNumber,
+        baseAmount: baseAmount,
+        discount: discount,
+        totalAmount: totalAmount,
+        taxApplicability: valid,
+        subcategoryId: id,
+      },
+    });
+    res.status(200).send({
+      msg: "Getting item",
+      status: 1,
+      data: itemdata,
     });
   } catch (err) {
     res.status(403).send({
